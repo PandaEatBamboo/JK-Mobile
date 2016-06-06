6a5e20c80f064e90930f26c44cbf3804d81942e6
ひろぱんだ <Hiro@hiropandas-MacBook-Pro.local>
2016-06-06 20:02:53 +0900

    Finish?

--- input.js
+++ input.js
@@ -1,5 +1,6 @@
 require("./src/SASS/animation.scss");
 require("./src/SASS/style.scss");
+require("./src/SASS/grade.scss");
 $ = require("jquery");
 Vue = require("vue");
 require("./src/JS/main.js");
